bd024a301b60ac11110c8d321a6bf1a69fa54092
Jan-Christoph Klie <gitkraken@jck.fastmail.com>
2017-11-08 21:10:09 +0800

    Refactor Set/Put to own opcodes

--- src/dvi/disassembler.rs
+++ src/dvi/disassembler.rs
@@ -31,9 +31,9 @@ impl Disassembler {
     }
 
     fn disassemble_next(&mut self) -> OpCode {
-        let byte = self.consume_byte();
+        let byte = self.consume_one_byte();
         match byte {
-            0...127 => OpCode::SetN { n: byte },
+            0...127 => self.handle_set_char(byte),
             128 => self.handle_set1(),
             129 => self.handle_set2(),
             130 => self.handle_set3(),
@@ -45,69 +45,74 @@ impl Disassembler {
             136 => self.handle_put4(),
             137 => self.handle_put_rule(),
             138 => self.handle_nop(),
+            139 => self.handle_bop(),
             _ => panic!("Unknown opcode: {}", byte),
         }
     }
 
+    fn handle_set_char(&mut self, byte: u32) -> OpCode {
+        OpCode::SetChar { n: byte }
+    }
+
     fn handle_set1(&mut self) -> OpCode {
-        OpCode::SetN {
-            n: self.consume_n_bytes(1),
+        OpCode::Set1 {
+            n: self.consume_one_byte(),
         }
     }
 
     fn handle_set2(&mut self) -> OpCode {
-        OpCode::SetN {
-            n: self.consume_n_bytes(2),
+        OpCode::Set2 {
+            n: self.consume_two_bytes(),
         }
     }
 
     fn handle_set3(&mut self) -> OpCode {
-        OpCode::SetN {
-            n: self.consume_n_bytes(3),
+        OpCode::Set3 {
+            n: self.consume_three_bytes(),
         }
     }
 
     fn handle_set4(&mut self) -> OpCode {
-        OpCode::SetN {
-            n: self.consume_n_bytes(4),
+        OpCode::Set4 {
+            n: self.consume_four_bytes(),
         }
     }
 
     fn handle_set_rule(&mut self) -> OpCode {
         OpCode::SetRule {
-            a: self.consume_n_bytes(4) as i32,
-            b: self.consume_n_bytes(4) as i32,
+            a: self.consume_four_bytes(),
+            b: self.consume_four_bytes(),
         }
     }
 
     fn handle_put1(&mut self) -> OpCode {
-        OpCode::PutN {
-            n: self.consume_n_bytes(1),
+        OpCode::Put1 {
+            n: self.consume_one_byte(),
         }
     }
 
     fn handle_put2(&mut self) -> OpCode {
-        OpCode::PutN {
-            n: self.consume_n_bytes(2),
+        OpCode::Put2 {
+            n: self.consume_two_bytes(),
         }
     }
 
     fn handle_put3(&mut self) -> OpCode {
-        OpCode::PutN {
-            n: self.consume_n_bytes(3),
+        OpCode::Put3 {
+            n: self.consume_three_bytes(),
         }
     }
 
     fn handle_put4(&mut self) -> OpCode {
-        OpCode::PutN {
-            n: self.consume_n_bytes(4),
+        OpCode::Put4 {
+            n: self.consume_four_bytes(),
         }
     }
 
     fn handle_put_rule(&mut self) -> OpCode {
         OpCode::PutRule {
-            a: self.consume_n_bytes(4) as i32,
-            b: self.consume_n_bytes(4) as i32,
+            a: self.consume_four_bytes(),
+            b: self.consume_four_bytes(),
         }
     }
 
@@ -115,8 +120,36 @@ impl Disassembler {
         OpCode::Nop
     }
 
-    fn consume_byte(&mut self) -> u32 {
+    fn handle_bop(&mut self) -> OpCode {
+        OpCode::Bop {
+            c0: self.consume_four_bytes(),
+            c1: self.consume_four_bytes(),
+            c2: self.consume_four_bytes(),
+            c3: self.consume_four_bytes(),
+            c4: self.consume_four_bytes(),
+            c5: self.consume_four_bytes(),
+            c6: self.consume_four_bytes(),
+            c7: self.consume_four_bytes(),
+            c8: self.consume_four_bytes(),
+            c9: self.consume_four_bytes(),
+            p: self.consume_four_bytes(),
+        }
+    }
+
+    fn consume_one_byte(&mut self) -> u32 {
         self.consume_n_bytes(1)
+    }
+
+    fn consume_two_bytes(&mut self) -> u32 {
+        self.consume_n_bytes(2)
+    }
+
+    fn consume_three_bytes(&mut self) -> u32 {
+        self.consume_n_bytes(3)
+    }
+
+    fn consume_four_bytes(&mut self) -> i32 {
+        self.consume_n_bytes(4) as i32
     }
 
     fn consume_n_bytes(&mut self, n: u32) -> u32 {
@@ -148,7 +181,7 @@ mod tests {
         for i in 0..127 {
             let result = disassemble(vec![i]);
 
-            assert_that_opcode_was_generated(result, OpCode::SetN { n: i as u32 })
+            assert_that_opcode_was_generated(result, OpCode::SetChar { n: i as u32 })
         }
     }
 
@@ -156,28 +189,28 @@ mod tests {
     fn test_disassemble_set1() {
         let result = disassemble(vec![128, 0xAB]);
 
-        assert_that_opcode_was_generated(result, OpCode::SetN { n: 0xAB })
+        assert_that_opcode_was_generated(result, OpCode::Set1 { n: 0xAB })
     }
 
     #[test]
     fn test_disassemble_set2() {
         let result = disassemble(vec![129, 0xAB, 0xCD]);
 
-        assert_that_opcode_was_generated(result, OpCode::SetN { n: 0xABCD })
+        assert_that_opcode_was_generated(result, OpCode::Set2 { n: 0xABCD })
     }
 
     #[test]
     fn test_disassemble_set3() {
         let result = disassemble(vec![130, 0xAB, 0xCD, 0xEF]);
 
-        assert_that_opcode_was_generated(result, OpCode::SetN { n: 0xABCDEF })
+        assert_that_opcode_was_generated(result, OpCode::Set3 { n: 0xABCDEF })
     }
 
     #[test]
     fn test_disassemble_set4() {
-        let result = disassemble(vec![131, 0xDE, 0xAD, 0xBE, 0xEF]);
+        let result = disassemble(vec![131, 0x00, 0x11, 0x22, 0x33]);
 
-        assert_that_opcode_was_generated(result, OpCode::SetN { n: 0xDEADBEEF })
+        assert_that_opcode_was_generated(result, OpCode::Set4 { n: 0x112233 })
     }
 
     #[test]
@@ -197,28 +230,28 @@ mod tests {
     fn test_disassemble_put1() {
         let result = disassemble(vec![133, 0xAB]);
 
-        assert_that_opcode_was_generated(result, OpCode::PutN { n: 0xAB })
+        assert_that_opcode_was_generated(result, OpCode::Put1 { n: 0xAB })
     }
 
     #[test]
     fn test_disassemble_put2() {
         let result = disassemble(vec![134, 0xAB, 0xCD]);
 
-        assert_that_opcode_was_generated(result, OpCode::PutN { n: 0xABCD })
+        assert_that_opcode_was_generated(result, OpCode::Put2 { n: 0xABCD })
     }
 
     #[test]
     fn test_disassemble_put3() {
         let result = disassemble(vec![135, 0xAB, 0xCD, 0xEF]);
 
-        assert_that_opcode_was_generated(result, OpCode::PutN { n: 0xABCDEF })
+        assert_that_opcode_was_generated(result, OpCode::Put3 { n: 0xABCDEF })
     }
 
     #[test]
     fn test_disassemble_put4() {
-        let result = disassemble(vec![136, 0xDE, 0xAD, 0xBE, 0xEF]);
+        let result = disassemble(vec![136, 0x00, 0x11, 0x22, 0x33]);
 
-        assert_that_opcode_was_generated(result, OpCode::PutN { n: 0xDEADBEEF })
+        assert_that_opcode_was_generated(result, OpCode::Put4 { n: 0x112233 })
     }
 
     #[test]
